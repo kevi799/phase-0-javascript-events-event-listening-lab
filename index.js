2ab0e54ca803e1d2f967ee4b1f3697603f258045
kelvinMulinge <kelvinmulinge9702@gmail.com>
2024-10-10 17:14:17 +0300

    Adds Eventlistner

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
+const input = document.getElementById('button');
 function addingEventListener() {
+    alert("You clicked Me");
+    input.addEventListener('click', addEventListener);
 }
+addingEventListener();
